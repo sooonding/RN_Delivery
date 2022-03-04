bb99ada41f5e3371eea21c5aa06b2ec4780a5b32
sooonding <sooonding@gmail.com>
2022-03-04 10:43:57 +0900

    feat(tutorial):
    - react-navigation 간단한 사용하기
    - android, ios setting
    - flipper setting

--- App.tsx
+++ App.tsx
@@ -1,115 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, {useCallback} from 'react';
+import {Text, TouchableHighlight, View, Pressable} from 'react-native';
 
-import React from 'react';
+import {NavigationContainer, ParamListBase} from '@react-navigation/native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// NOTE Stack을 부르는것은 그냥 외우자
+const Stack = createNativeStackNavigator();
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
 };
+
+// NOTE: 해당 type은 react-navigation에서 참조하면 된다.
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
+
+function HomeScreen({navigation}: HomeScreenProps) {
+  const onClick = useCallback(() => {
+    //NOTE: 페이지를 이동시키는 역할로 navigate메서드 인자는 screen의 네임과 동일시 이동합니다.
+    navigation.navigate('Details');
+  }, [navigation]);
+
+  const goDetail = useCallback(() => {
+    navigation.navigate('Details');
+  }, [navigation]);
+
+  return (
+    <>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'yellow',
+          alignItems: 'flex-end',
+          justifyContent: 'center',
+        }}>
+        <Pressable
+          onPress={onClick}
+          style={{
+            paddingVertical: 20,
+            paddingHorizontal: 20,
+            backgroundColor: 'red',
+          }}>
+          <Text style={{color: 'white'}}>Home Screen</Text>
+        </Pressable>
+      </View>
+      <View
+        style={{
+          flex: 5,
+          backgroundColor: 'blue',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+        }}>
+        <Pressable onPress={goDetail}>
+          <Text>Two Screen</Text>
+        </Pressable>
+      </View>
+    </>
+  );
+}
+
+function DetailsScreen({navigation}: DetailsScreenProps) {
+  const onClick = useCallback(() => {
+    navigation.navigate('Home');
+  }, [navigation]);
+
+  return (
+    <>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <TouchableHighlight onPress={onClick}>
+          <Text>Details Screen</Text>
+        </TouchableHighlight>
+      </View>
+    </>
+  );
+}
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      {/* NOTE Navigator가 screen을 그룹으로 묶어준다. (page의 묶음) */}
+      <Stack.Navigator initialRouteName="Home">
+        {/* NOTE screen은 화면(page)이라고 보면 된다. */}
+        {/* NOTE: 해당 Screen이 해당 컴포넌트에 props로 navigation과route를 자동으로 전달합니다. */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: '홈화면'}}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
