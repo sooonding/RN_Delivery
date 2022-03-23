da511ddc79b2025ad0015d53b54ce87d5dfdff0e
sooonding <sooonding@gmail.com>
2022-03-23 18:35:21 +0900

    feat(geolocation&camera): 지오로케이션&카메라
    - react-native-image-crop-picker & react-native-image-resizer 라이브러리를 통한 카메라 촬영 및 이미지 갤러리 추가
    - @react-native-community/geolocation 라이브러리로 현재 장소의 로케이션 지정

--- src/pages/Complete.tsx
+++ src/pages/Complete.tsx
@@ -1,10 +1,150 @@
-import {Text, SafeAreaView} from 'react-native';
-import React from 'react';
+import {
+  Alert,
+  Dimensions,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {useAppDispatch} from '../store';
+import {
+  NavigationProp,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
+import {LoggedInParamList} from '../../App';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import ImageResizer from 'react-native-image-resizer';
+import ImagePicker from 'react-native-image-crop-picker';
 
 export default function Complete() {
+  const dispatch = useAppDispatch();
+  const route = useRoute<RouteProp<LoggedInParamList>>();
+  const navigation = useNavigation<NavigationProp<LoggedInParamList>>();
+  const [image, setImage] =
+    useState<{uri: string; name: string; type: string}>();
+  const [preview, setPreview] = useState<{uri: string}>();
+  const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const orderId = route.params?.orderId;
+
+  const onResponse = useCallback(async response => {
+    console.log(response.width, response.height, response.exif, 'OPEN');
+    setPreview({uri: `data:${response.mime};base64,${response.data}`});
+    const orientation = (response.exif as any)?.Orientation;
+    console.log('orientation', orientation);
+    return ImageResizer.createResizedImage(
+      response.path,
+      600,
+      600,
+      response.mime.includes('jpeg') ? 'JPEG' : 'PNG',
+      100,
+      0,
+    ).then(r => {
+      console.log(r.uri, r.name);
+
+      setImage({
+        uri: r.uri,
+        name: r.name,
+        type: response.mime,
+      });
+    });
+  }, []);
+
+  const onChangeFile = useCallback(() => {
+    return ImagePicker.openPicker({
+      includeExif: true,
+      includeBase64: true,
+      mediaType: 'photo',
+    })
+      .then(onResponse)
+      .catch(console.log);
+  }, [onResponse]);
+
+  const onTakePhoto = useCallback(() => {
+    console.log('이미지 촬영 클릭!');
+    return ImagePicker.openCamera({
+      width: 300,
+      height: 400,
+      includeBase64: true, //NOTE: 미리보기 표시를 위함
+      includeExif: true, //NOTE: 핸드폰 위치 방향
+      // cropping: true,
+      // saveToPhotos: true,
+    })
+      .then(onResponse)
+      .catch(console.log);
+  }, [onResponse]);
+
   return (
     <SafeAreaView>
-      <Text>완료</Text>
+      <View style={styles.orderId}>
+        <Text style={styles.orderIdText}>주문번호: {orderId}</Text>
+      </View>
+      <View style={styles.preview}>
+        {preview && <Image style={styles.previewImage} source={preview} />}
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Pressable style={styles.button} onPress={onTakePhoto}>
+          <Text style={styles.buttonText}>이미지 촬영</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={onChangeFile}>
+          <Text style={styles.buttonText}>이미지 선택</Text>
+        </Pressable>
+        <Pressable
+          style={
+            image
+              ? styles.button
+              : StyleSheet.compose(styles.button, styles.buttonDisabled)
+          }>
+          <Text style={styles.buttonText}>완료</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  orderId: {
+    margin: 15,
+  },
+  orderIdText: {
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#6667AB',
+  },
+  preview: {
+    marginHorizontal: 10,
+    width: Dimensions.get('window').width - 20,
+    height: Dimensions.get('window').height / 3,
+    backgroundColor: 'rgba(62, 62, 62, 0.3)',
+    marginBottom: 10,
+  },
+  previewImage: {
+    height: Dimensions.get('window').height / 3,
+    resizeMode: 'contain', //NOTE:
+  },
+  buttonWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 10,
+  },
+  button: {
+    width: 110,
+    fontSize: 18,
+    alignItems: 'center',
+    backgroundColor: '#e2c1c0',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  buttonDisabled: {
+    backgroundColor: '#6667AB',
+  },
+});
--- src/pages/Ing.tsx
+++ src/pages/Ing.tsx
@@ -1,10 +1,143 @@
-import {Text, SafeAreaView} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Dimensions, Text, View} from 'react-native';
+import NaverMapView, {Circle, Marker, Path} from 'react-native-nmap';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
+import Geolocation from '@react-native-community/geolocation';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {LoggedInParamList} from '../../App';
+import {instanceOf} from 'prop-types';
 
-export default function Ing() {
+type IngScreenProps = NativeStackScreenProps<LoggedInParamList, 'Delivery'>;
+
+function Ing({navigation}: IngScreenProps) {
+  console.dir(navigation);
+  const deliveries = useSelector((state: RootState) => state.order.deliveries);
+  const [myPosition, setMyPosition] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+
+  useEffect(() => {
+    //NOTE: .watchPosition도 있다.(위치가 바뀔 때 마다 알려주게)
+    Geolocation.getCurrentPosition(
+      info => {
+        console.log(info, '현재위치');
+        setMyPosition({
+          latitude: info.coords.latitude,
+          longitude: info.coords.longitude,
+        });
+      },
+      console.error,
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        //distanceFilter : 해당 콜백에 실행되게 하겠다.
+        distanceFilter: 100,
+      },
+    );
+  }, []);
+
+  if (!deliveries?.[0]) {
+    return (
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <Text>주문을 먼저 수락해주세요!</Text>
+      </View>
+    );
+  }
+
+  if (!myPosition || !myPosition.latitude) {
+    return (
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <Text>내 위치를 로딩 중입니다. 권한을 허용했는지 확인해주세요.</Text>
+      </View>
+    );
+  }
+
+  const {start, end} = deliveries?.[0];
+
   return (
-    <SafeAreaView>
-      <Text>지도</Text>
-    </SafeAreaView>
+    <View>
+      <View
+        style={{
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+        }}>
+        <NaverMapView
+          style={{width: '100%', height: '100%'}}
+          zoomControl={true}
+          nightMode={true}
+          center={{
+            zoom: 10,
+            tilt: 50,
+            latitude: (start.latitude + end.latitude) / 2,
+            longitude: (start.longitude + end.longitude) / 2,
+          }}>
+          {myPosition?.latitude && (
+            // 내위치를 알려주는
+            <Marker
+              coordinate={{
+                latitude: myPosition.latitude,
+                longitude: myPosition.longitude,
+              }}
+              width={25}
+              height={25}
+              anchor={{x: 0.5, y: 0.5}}
+              caption={{text: '나'}}
+              image={require('../assets/riderman.png')}
+            />
+          )}
+          {myPosition?.latitude && (
+            <Path
+              coordinates={[
+                {
+                  latitude: myPosition.latitude,
+                  longitude: myPosition.longitude,
+                },
+                {latitude: start.latitude, longitude: start.longitude},
+              ]}
+              color="orange"
+            />
+          )}
+          <Circle coordinate={start} color={'rgba(255,0,0,0.3)'} radius={200} />
+          <Marker
+            coordinate={{
+              latitude: start.latitude,
+              longitude: start.longitude,
+            }}
+            width={15}
+            height={15}
+            anchor={{x: 0.5, y: 0.5}}
+            caption={{text: '출발'}}
+            image={require('../assets/blue-dot.png')}
+          />
+          <Path
+            coordinates={[
+              {
+                latitude: start.latitude,
+                longitude: start.longitude,
+              },
+              {latitude: end.latitude, longitude: end.longitude},
+            ]}
+            color="orange"
+          />
+          <Circle coordinate={end} color={'blue'} radius={200} />
+          <Marker
+            coordinate={{latitude: end.latitude, longitude: end.longitude}}
+            width={15}
+            height={15}
+            anchor={{x: 0.5, y: 0.5}}
+            caption={{text: '도착'}}
+            image={require('../assets/green-dot.png')}
+            onClick={() => {
+              console.log(navigation);
+              navigation.push('Complete', {orderId: deliveries[0].orderId});
+            }}
+          />
+        </NaverMapView>
+      </View>
+    </View>
   );
 }
+
+export default Ing;
